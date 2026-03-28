894b9a7cc5f4736feb503702c2ddd718651110e3
Malik Zeeshan <malikzeeshannb@gmail.com>
2026-03-28 19:21:26 +0300

    Add files via upload

---
+++ modules/what-java-script-is/apps/app.js
@@ -0,0 +1,5 @@
+console.log("this is my first log here");
+console.log("i am a front end student");
+console.log("5 + 10");
+console.log("my first name is malik");
+console.log(true, false,"JS is fun");
